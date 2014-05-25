26cd6100d96f3f0259957795445329024d020229
leeks <leeks@google.com>
2014-05-25 22:08:21 +0900

    Apply Log4j2.xml

--- src/main/java/com/creamsugardonut/annotationconfig/controller/HomeController.java
+++ src/main/java/com/creamsugardonut/annotationconfig/controller/HomeController.java
@@ -1,31 +1,32 @@
 package com.creamsugardonut.annotationconfig.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 @Controller
-public class HomeController
-{
-    //private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-    
-    @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST })
-    public String home(Locale locale, Model model)
-    {
-        //logger.info("Welcome home! The client locale is {}.", locale);
-        
+public class HomeController {
+
+    private static Logger logger = LogManager.getLogger(HomeController.class.getName());
+
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    public String home(Locale locale, Model model) {
+        logger.info("Welcome home! The client locale is {}.", locale);
+
         Date date = new Date();
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-        
+
         String formattedDate = dateFormat.format(date);
-        
-        model.addAttribute("serverTime", formattedDate );
-        
+
+        model.addAttribute("serverTime", formattedDate);
+
         return "home";
     }
 }
